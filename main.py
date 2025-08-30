a3938230d813450a94fa0019c13764b0910399e4
Brody Roth <brodyroth23@gmail.com>
2025-08-29 23:40:24 -0400

    Adding YOLO Language Model to test object detection with OpenCV.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from ultralytics import YOLO
 
 cap = cv2.VideoCapture(0)
 
@@ -8,16 +9,18 @@ def main():
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 40)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 50)
 
+    model = YOLO("yolov8n.pt")
+
     while True:
         ret, frame = cap.read()
         if not ret:
             break
 
-        cv2.imshow('frame', frame)
+        results = model(frame)[0]
 
-        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        
+        annotated_frame = results.plot()
+
+        cv2.imshow("YOLOv8 Detection", annotated_frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
